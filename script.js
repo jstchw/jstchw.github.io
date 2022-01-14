20337257a0d49cb1e41186c17db89c45d1a6abad
Artem <bobr703@gmail.com>
2022-01-14 13:27:04 +0000

    pushState to replaceState

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 function next() {
     currentUrl = window.location.href
-    alert(currentUrl)
     if(currentUrl = 'index.html') nextUrl = '/index2.html'
-    history.pushState('', 'Продолжай', nextUrl)
+    history.replaceState('', 'Продолжай', nextUrl)
 }
