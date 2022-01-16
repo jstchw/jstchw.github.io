f02109b324cc32b55b0d6239253e218ca4787c59
Artem <bobr703@gmail.com>
2022-01-16 20:22:21 +0000

    Final?

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function validateForm() {
     let x = document.forms["lastname"]["text"].value;
 
     if(x == "Сироткин") {
-        setTimeout(function() {window.location = "/index2.html" });
+        setTimeout(function() {window.location = "/you_got_it.html" });
     }
     else {
         alert("Используй шифр Атбаш!")
