179201362843a0691e278590275cfb468c0d84b3
Artem <bobr703@gmail.com>
2022-03-14 18:37:27 +0000

    New page

---
+++ repo.js
@@ -0,0 +1,27 @@
+const start = new Date('2022-02-14T23:00:00')
+let now = new Date()
+
+document.querySelectorAll('.dating-since').forEach(function(el) {
+    el.innerHTML = getDiffDays();
+});
+
+if (/[1$]$/.test(getDiffDays())) {
+    document.querySelectorAll('.days').forEach(function(el) {
+        el.innerHTML = ' день';
+    });
+} else if(/[234]$/.test(getDiffDays())) {
+    document.querySelectorAll('.days').forEach(function(el) {
+        el.innerHTML = ' дня';
+    });
+} else if(/[567890]$/.test(getDiffDays())){
+    document.querySelectorAll('.days').forEach(function(el) {
+        el.innerHTML = ' дней';
+    });
+}
+
+
+function getDiffDays() {
+    const oneDay = 1000 * 60 * 60 * 24
+    return Math.round((now.getTime() - start.getTime()) / oneDay).toString()
+
+}
